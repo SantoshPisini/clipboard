5988796ea1de181e169e98249c6c8ff972cbb8fb
Santosh Pisini <>
2023-03-26 12:48:04 +0530

    Web app setup

---
+++ web/src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import logo from "./assets/logo512.png";
+import { Layout } from "antd";
+const { Header, Footer, Content } = Layout;
+
+function App() {
+  return (
+    <Layout>
+      <Header className="header">
+        <img src={logo} alt="Clipboard logo" className="header-logo" />
+        <h1 className="header-title">Clipboard</h1>
+      </Header>
+      <Content className="content"></Content>
+      <Footer>Clipboard - By Santosh Pisini.</Footer>
+    </Layout>
+  );
+}
+
+export default App;
